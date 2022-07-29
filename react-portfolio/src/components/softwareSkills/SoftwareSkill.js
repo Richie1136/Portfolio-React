75e54218cbc88ad77545a5334430cf1536ada313
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-29 11:37:08 -0400

    Cleaning up code

--- react-portfolio/src/components/softwareSkills/SoftwareSkill.js
+++ react-portfolio/src/components/softwareSkills/SoftwareSkill.js
@@ -3,27 +3,25 @@ import "./SoftwareSkill.css";
 import { skillsSection } from "../../portfolio";
 
 
-const SoftwareSkill = () => {
-  return (
-    <div>
-      <div className="software-skills-main-div">
-        <ul className="dev-icons">
-          {skillsSection.softwareSkills.map((skills, i) => {
-            return (
-              <li
-                key={i}
-                className="software-skill-inline"
-                name={skills.skillName}
-              >
-                <i className={skills.fontAwesomeClassname}></i>
-                <p>{skills.skillName}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+const SoftwareSkill = () => (
+  <div>
+    <div className="software-skills-main-div">
+      <ul className="dev-icons">
+        {skillsSection.softwareSkills.map((skills, i) => {
+          return (
+            <li
+              key={i}
+              className="software-skill-inline"
+              name={skills.skillName}
+            >
+              <i className={skills.fontAwesomeClassname}></i>
+              <p>{skills.skillName}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  </div>
+)
 
 export default SoftwareSkill
